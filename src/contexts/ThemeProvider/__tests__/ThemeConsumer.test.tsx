a241b163cbf65ae1a3e67e7ef20a03e8d70381f1
Andre <mail@andrejilderda.nl>
2021-12-14 19:39:58 +0100

    test: Base testing utils + todo's

---
+++ src/contexts/ThemeProvider/__tests__/ThemeConsumer.test.tsx
@@ -0,0 +1,6 @@
+import ThemeConsumer from '../ThemeConsumer';
+
+describe('ThemeConsumer', () => {
+  test.todo('An error is thrown when used outside a ThemeProvider');
+  test.todo('Props are begin forwarded properly');
+});
---
+++ src/contexts/ThemeProvider/__tests__/ThemeProvider.test.tsx
@@ -0,0 +1,6 @@
+import ThemeProvider from '../ThemeProvider';
+
+describe('ThemeProvider', () => {
+  test.todo('ThemeProvider nesting');
+  test.todo('The window blur classname is applied on blur event');
+});
