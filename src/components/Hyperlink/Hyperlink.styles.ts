51c3523b397db5bb8865b4a6aad3b9c7fc230fe4
Andre <mail@andrejilderda.nl>
2022-01-01 21:01:13 +0100

    chore: Remove outdated boilerplate code for Hyperlink

--- src/components/Hyperlink/Hyperlink.styles.ts
+++
@@ -1,9 +0,0 @@
-import createStyles from 'src/theme/createStyles';
-import { ComponentStyles } from 'src/theme/types';
-// import focusableVariants from 'src/styles/focusableVariants';
-
-const styles: ComponentStyles = {
-  wrapper: {},
-};
-
-export default createStyles<keyof typeof styles>('hyperlink', styles);
