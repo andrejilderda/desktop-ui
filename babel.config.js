1c1bcc677d907798cb613dc4e0cf403672ee79cb
Andre <mail@andrejilderda.nl>
2022-03-31 22:34:43 +0200

    chore: Fix vanilla-extract references in Jest

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { targets: { node: 12 } }],
+    '@babel/preset-typescript',
+    '@babel/preset-react',
+  ],
+
+  overrides: [
+    {
+      include: [
+        './packages/css',
+        './packages/dynamic',
+        './packages/private',
+        './packages/recipes',
+        './packages/sprinkles',
+        './packages/utils',
+      ],
+      presets: [['@babel/preset-env', { targets: { esmodules: true } }]],
+    },
+  ],
+};
--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,11 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleNameMapper: {
+    '^lib/(.*)': '<rootDir>/packages/lib/$1',
+  },
+  transform: {
+    '\\.tsx?$': ['babel-jest', { configFile: './babel-jest.config.js' }],
+  },
+  plugins: [require.resolve('@vanilla-extract/babel-plugin')],
 };
