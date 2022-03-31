588b9f114221eaaa0876f0f5ab55a19f87d8cdb1
Andre <mail@andrejilderda.nl>
2022-03-31 21:53:00 +0200

    chore: Fix jest test setup

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,5 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  transformIgnorePatterns: ['./node_modules/', './dist/'],
-  moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/fileMock.js',
-    '\\.(css|less|scss)$': 'identity-obj-proxy',
-  },
-  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+  testEnvironment: 'node',
 };
