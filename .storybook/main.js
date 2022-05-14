9fb17fb3541a917becebe90368fc4bac1d08f0b6
Andre <mail@andrejilderda.nl>
2022-05-14 20:57:47 +0200

    chore: Compose Storybooks of styles & React library

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,16 @@
 module.exports = {
   stories: [],
-  addons: ['@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
+  ],
   // uncomment the property below if you want to apply some webpack config globally
   // webpackFinal: async (config, { configType }) => {
   //   // Make whatever fine-grained changes you need that should apply to all storybook configs
