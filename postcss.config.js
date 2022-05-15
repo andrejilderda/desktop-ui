d5331e88447a81229c1ff4e5eb265c101fa7de7e
Andre <mail@andrejilderda.nl>
2022-05-15 20:23:26 +0200

    feat: Add postcss-design-tokens

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
+const tokens = require('./libs/styles/src/lib/tokens/index.js');
+
 module.exports = {
   plugins: {
     'postcss-preset-env': {},
     'postcss-nested': {},
+    'postcss-design-tokens': { tokens },
   },
 };
