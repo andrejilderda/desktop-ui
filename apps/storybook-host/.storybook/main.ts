b5323b544d788f66a22a4fa62b1e2c93f6ce7db3
Andre <mail@andrejilderda.nl>
2023-02-26 21:26:19 +0100

    chore: change Storybook composition refs

--- apps/storybook-host/.storybook/main.ts
+++ apps/storybook-host/.storybook/main.ts
@@ -14,15 +14,31 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
-  refs: {
-    react: {
-      title: "React",
-      url: "http://localhost:6007",
-    },
-    styles: {
-      title: "Styles",
-      url: "http://localhost:6008",
-    },
+  refs: (_config, { configType }) => {
+    console.log("configType", configType);
+    if (configType === "DEVELOPMENT") {
+      return {
+        react: {
+          title: "React",
+          url: "http://localhost:6007",
+        },
+        styles: {
+          title: "Styles",
+          url: "http://localhost:6008",
+        },
+      };
+    }
+
+    return {
+      react: {
+        title: "React",
+        url: "https://desktop-ui-react.netlify.app/",
+      },
+      styles: {
+        title: "Styles",
+        url: "https://desktop-ui-styles.netlify.app/",
+      },
+    };
   },
 };
 export default config;
