7944a35ce36d6a1c474a3c0b2590c857958361e3
Andre <mail@andrejilderda.nl>
2023-03-13 20:35:56 +0100

    chore: update dependencies

--- packages/react/storybook/.storybook/main.ts
+++ packages/react/storybook/.storybook/main.ts
@@ -1,22 +1,16 @@
 import type { StorybookConfig } from "@storybook/react-vite";
-
 const config: StorybookConfig = {
-  // stories: ["./../**/*.mdx", "./../../**/*.stories.@(js|jsx|ts|tsx)"],
-  stories: [
-    // There's an issue where the glob is not being resolved correctly and is
-    // matching files outside of the given directory:
-    // https://github.com/storybookjs/storybook/issues/21031
-    // This is a workaround for now.
-    "../../button/src/*.stories.@(js|jsx|ts|tsx)",
-  ],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
+  // There's an issue where providing an ignore glob is not working:
+  // https://github.com/storybookjs/storybook/issues/11181#issuecomment-1455449562
+  // stories: ["./../../**/*.mdx", "./../../**!(/node_modules)/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["./../../**/src/*.mdx", "./../../**/src/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
   framework: {
     name: "@storybook/react-vite",
-    options: {},
+    options: {}
   },
+  docs: {
+    autodocs: true
+  }
 };
 export default config;
